138ad9e6a3a5e894e7af192a1912c8b915c564d3
Alex <alexanderjrix@gmail.com>
2018-06-14 09:45:08 -0500

    Update bic.R with new way to calcluate the BIC scores of SEMs.  Update DESCRIPTION

--- R/bic.R
+++ R/bic.R
@@ -1,19 +1,46 @@
-score_graph <- function(cgraph, data) {
-  if(!is.cgraph(cgraph))
-    stop("Graph is not a causality graph!")
-  if(!is.dag(cgraph) & !is.pattern(cgraph))
-    stop("Graph must be a dag or pattern!")
-  if(is.pattern(cgraph))
-    cgraph <- as.dag(cgraph)
+# score_graph <- function(cgraph, data) {
+#   if(!is.cgraph(cgraph))
+#     stop("Graph is not a causality graph!")
+#   if(!is.dag(cgraph) & !is.pattern(cgraph))
+#     stop("Graph must be a dag or pattern!")
+#   if(is.pattern(cgraph))
+#     cgraph <- as.dag(cgraph)
+#
+#   parents <- parents(cgraph)
+#   sum_BIC <- 0
+#   for (node in names(parents)) {
+#     form <- paste(node, "~", paste(parents[[node]], collapse = "+"))
+#     form <- as.formula(form)
+#     sum_BIC <- sum_BIC + BIC(lm(form, data)) + 2*length(parents[[node]]) + 1 #bias correction
+#   }
+#   return(sum_BIC)
+# }
 
+score_graph <- function(cgraph, data) {
+  if (!is.cgraph(cgraph))
+    stop("Graph is not a causality graph!")
+  if (!is.dag(cgraph)) {
+    if (is.pattern(cgraph))
+      cgraph <- as.dag(cgraph) {
+      if (is.null(cgraph)) {
+        warning("Cannot score graph because it lacks a DAG extesnion")
+        return(NA)
+      }
+    }
+  }
   parents <- parents(cgraph)
   sum_BIC <- 0
   for (node in names(parents)) {
-    form <- paste(node, "~", paste(parents[[node]], collapse = "+"))
-    form <- as.formula(form)
-    sum_BIC <- sum_BIC + BIC(lm(form, data)) + 2*length(parents[[node]]) + 1 #bias correction
+    ssq   <- cov(data[node])
+    node.parents <- parents[[node]]
+    COVXX <- cov(data[node.parents])
+    COVXY <- as.vector(cov(data[node], data[node.parents]))
+    ssq   <- ssq - COVXY %*% ginv(COVXX) %*% COVXY
+    theta <- length(node.parents) + 1
+    n <- nrow(data)
+    sum_BIC <- sum_BIC + n*log(unname(ssq)) + theta*log(n)
   }
-  return(sum_BIC)
+  return(as.numeric(sum_BIC))
 }
 
 parents <- function(cgraph) {
