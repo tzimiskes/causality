9a296bac0baf8d3ac714e6889d77c8a170cb4746
tzimiskes <alexanderjrix@gmail.com>
2018-06-18 13:31:45 -0500

    document lavaan.R, add some type checking

--- R/lavaan.R
+++ R/lavaan.R
@@ -1,35 +1,25 @@
-# score_graph <- function(cgraph, data) {
-#   if (!is.cgraph(cgraph))
-#     stop("Graph is not a causality graph!")
-#   if (!is.dag(cgraph) & !is.pattern(cgraph))
-#     stop("Graph must be a dag or pattern!")
-#   if (is.pattern(cgraph))
-#     cgraph <- as.dag(cgraph)
-#   parents <- parents(cgraph)
-#   sum_BIC <- 0
-#   for (node in names(parents)) {
-#     ssq   <- cov(data[node])
-#     node.parents <- parents[[node]]
-#     COVXX <- cov(data[node.parents])
-#     COVXY <- as.vector(cov(data[node], data[node.parents]))
-#     ssq   <- ssq - COVXY %*% ginv(COVXX) %*% COVXY
-#     theta <- length(node.parents) + 1
-#     n <- nrow(data)
-#     sum_BIC <- sum_BIC + n*log(unname(ssq)) + theta*log(n)
-#   }
-#   return(as.numeric(sum_BIC))
-# }
-
-
-
-
+#' Turn a causality graph into a lavaan formula
+#'
+#' Use this function to convert a causality.graph into a lavaan formula so you
+#' can fit the model to the data and assess the quality of the model from the
+#' graph.
+#' @param cgraph A causality.graph to be turned into a lavaan formula
+#' @details currently, only DAGs are supported, though patterns and PDAGs with
+#'   DAG extensions are accepted. Future work will inlcude PAGs, and including
+#'   output from the FnFC family as a latent measurement model.
+#' @return a \code{lavaan} formula, or an error
+#' @examples TODO(arix)
 as.lavaan.formula <- function(cgraph) {
-
-  cgraph <- as.dag(cgraph)
+  if (!is.cgraph(cgraph)) {
+    stop("Input is not a cgraph")
+  }
+  if (!is.dag(cgraph))
+    cgraph <- as.dag(cgraph)
   parents <- parents(cgraph)
   formulas <- c()
   for (node in names(parents)) {
-    formulas <- c(formulas, paste(node, "~", paste(parents[[node]], collapse = " + ")))
+    formulas <- c(formulas,
+                  paste(node, "~", paste(parents[[node]], collapse = " + ")))
   }
   return(paste(formulas, collapse = " ; "))
 }
