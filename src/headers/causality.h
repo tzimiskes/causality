476545e9110a11305254cc83211847cf9f82f979
tzimiskes <alexanderjrix@gmail.com>
2018-07-25 11:47:06 -0500

    fix broken merge and delete old topological sort function

--- src/headers/causality.h
+++ src/headers/causality.h
@@ -10,24 +10,17 @@
 #include <string.h>
 #include <stdio.h>
 #include <math.h>
-<<<<<<< HEAD
-=======
-
-
->>>>>>> e4e15321f005873841021e28e61ad585006b62f7
 
 #define NODES 0
 #define ADJACENCIES 1
 #define EDGES 2
 #define EDGES_NCOL 3
 
-<<<<<<< HEAD
 int * calculate_edges_ptr(SEXP Graph);
 SEXP calculate_edges_from_ptr(int * edges_ptr, SEXP Graph);
 int * ccf_topological_sort(SEXP Graph);
-=======
+
 int* calculate_edges_ptr(SEXP Graph);
 SEXP cf_topological_sort(SEXP dag);
 
->>>>>>> e4e15321f005873841021e28e61ad585006b62f7
 #endif
