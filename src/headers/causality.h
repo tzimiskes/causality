e4e15321f005873841021e28e61ad585006b62f7
Alex <alexanderjrix@gmail.com>
2018-07-25 09:19:18 -0500

    reformat causality.h

--- src/headers/causality.h
+++ src/headers/causality.h
@@ -6,18 +6,19 @@
 #include <Rinternals.h>
 
 // C LIBS
-#include<stdlib.h>
-#include<string.h>
-#include<stdio.h>
-#include<math.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include <math.h>
+
+
 
 #define NODES 0
 #define ADJACENCIES 1
 #define EDGES 2
 #define EDGES_NCOL 3
 
-
 int* calculate_edges_ptr(SEXP Graph);
 SEXP cf_topological_sort(SEXP dag);
-#endif
 
+#endif
