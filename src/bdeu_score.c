0a01f67b624061a7ffd02c1f1aa4bc4e0d92e1bf
tzimiskes <alexanderjrix@gmail.com>
2018-08-27 10:00:13 -0500

    fixed some bugs so bdeu_score outputs a score now. I have no idea ifi t is correct. Need to write test

--- src/bdeu_score.c
+++ src/bdeu_score.c
@@ -4,7 +4,7 @@
 
 
 
-static inline double calc_structure_prior(int n_par, int n_var, int structure_prior);
+static inline double calc_structure_prior(int n_par, int n_var, double structure_prior);
 
 double bdeu_score(dataframe data, int * xy, int n_par, double * fargs, int * iargs) {
   double sample_prior    = fargs[0];
@@ -22,16 +22,17 @@ double bdeu_score(dataframe data, int * xy, int n_par, double * fargs, int * iar
   int   n_y_states  = 1;
   for(int i = 0; i < n_par; ++i)
     n_y_states *= y_states[i];
-  /* create a matrix that will store the frequencies of state (y, x) */
+  /* create a matrix that will store the frequencies of state (y, x).
+   * The matrix will be indexed by row major format */
   int * alloced_mem = calloc((n_x_states + 1) * n_y_states, sizeof(int));
   int * n_jk = alloced_mem;
-  int * n_j  = alloced_mem + n_x_states * n_x_states;
+  int * n_j  = alloced_mem + n_x_states * n_y_states;
   /* unsure if y_vals should be allocated on the heap or stack */
   int y_state [n_par];
   for(int i = 0; i < n_obs; ++i) {
     int x_state = x[i];
-    for(int j = 1; j < n_par; ++j) {
-      y_state[j] = df[j][i];
+    for(int j = 0; j < n_par; ++j) {
+      y_state[j] = df[j + 1][i];
     }
     int k = 0;
     for (int j = 0; j < n_par; ++j) {
@@ -43,20 +44,19 @@ double bdeu_score(dataframe data, int * xy, int n_par, double * fargs, int * iar
   }
   double cell_prior = sample_prior / (n_x_states * n_y_states);
   double row_prior  = sample_prior / n_y_states;
-  double score = calc_structure_prior(n_par, n_obs, structure_prior);
+  double score      = structure_prior;
   for(int i = 0; i < n_y_states; ++i) {
     score -= lgamma(row_prior + n_j[i]);
     for(int j = 0; j < n_x_states; ++j)
       score += lgamma(cell_prior + n_jk[j + i*n_x_states]);
   }
   score += n_y_states * lgamma(row_prior);
-  score -= n_x_states * n_x_states * lgamma(cell_prior);
+  score -= n_x_states * n_y_states * lgamma(cell_prior);
   free(alloced_mem);
-  Rprintf("score: %f\n", score);
   return score;
 }
 
-static inline double calc_structure_prior(int n_par, int n_var, int structure_prior) {
+static inline double calc_structure_prior(int n_par, int n_var, double structure_prior) {
   return n_par * log(structure_prior/(n_var - 1)) + (n_var - n_par) *
            log(1.0f - (structure_prior/(n_var - 1)));
 }
