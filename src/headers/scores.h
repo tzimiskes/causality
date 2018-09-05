0c55714c172e9f853e326370b02ced0614629945
tzimiskes <alexanderjrix@gmail.com>
2018-09-05 15:30:40 -0500

    reformat scores.h and score.c
    fges needs to be updated regarding function pointer

--- src/headers/scores.h
+++ src/headers/scores.h
@@ -5,23 +5,29 @@
 #define BIC_SCORE "BIC"
 #define BDEU_SCORE "BDeu"
 
+typedef double (* score_func)(dataframe df,
+                              int      *xy,
+                              int       npar,
+                              double   *fargs,
+                              int      *iargs);
+
 double bdeu_score(dataframe data,
-                  int * xy,
-                  int n_par,
-                  double * fargs,
-                  int * iargs);
+                  int    *xy,
+                  int     npar,
+                  double *fargs,
+                  int    *iargs);
 double bic_score(dataframe data,
-                 int * xy,
-                 int n_par,
-                 double * fargs,
-                 int * iargs);
+                 int      *xy,
+                 int       npar,
+                 double   *fargs,
+                 int      *iargs);
 
 double score_diff(dataframe df,
-                  int * new_xy,
-                  int * old_xy,
-                  int new_n_par,
-                  int old_n_par,
-                  double * fargs,
-                  int * iargs,
-                  double (* score_fp)(dataframe, int *, int, double *, int *));
+                  int       *new_xy,
+                  int       *old_xy,
+                  int        new_npar,
+                  int        old_npar,
+                  double    *fargs,
+                  int       *iargs,
+                  score_func score);
 #endif
--- src/score.c
+++ src/score.c
@@ -4,109 +4,102 @@
 
 #include <benchmarkr.h>
 
-
-
 double score_graph(cgraph_ptr cg_ptr, dataframe d, double * fargs, int * iargs,
-  double (* score_fp)(dataframe, int *, int, double *, int *));
+  score_func score);
 
-SEXP ccf_score_graph_wrapper(SEXP Graph, SEXP Df, SEXP ScoreType,
-  SEXP States, SEXP FloatingArgs, SEXP IntegerArgs)
-  {
-  int * edges         = calculate_edges_ptr(Graph);
-  int n_nodes         = length(VECTOR_ELT(Graph, NODES));
-  int n_edges         = nrows(VECTOR_ELT(Graph, EDGES));
-  cgraph_ptr cg_ptr   = create_cgraph(n_nodes);
-  fill_in_cgraph(cg_ptr, n_edges, edges);
-  free(edges);
-  /* calcluate the integer arguments and floating point arguments for the
-   * score function. */
-  int * iargs;
-  if(!isNull(IntegerArgs))
-    iargs = INTEGER(IntegerArgs);
-  else
-    iargs = NULL;
-  double * fargs;
-  if(!isNull(FloatingArgs))
-    fargs = REAL(FloatingArgs);
-  else
-    fargs = NULL;
-  /* states stores the number of states (in the sense of degrees of freedom)
-   * of each variable in the data frame with the caveat that for continuous
-   * variables, the number of states is considered to be to be 0 instead of 1.
-   * This allows me to use the actual value in each entry of dims to determine
-   * the type (real or discrete/integer) of the variable in the data frame.
-   * Instead, we store the columns as void pointers in df. This helps divorce C
-   * and R so it is easier to port this package to python, julia, etc. */
-  dataframe df;
-  df.n_var  = length(Df);
-  df.n_obs  = length(VECTOR_ELT(Df, 0));
-  df.states = INTEGER(States);
-  df.df     = malloc(df.n_var * sizeof(void *));
-  /* populate df with the pointers to the columns of the R dataframe */
-  int * states = df.states;
-  for(int i = 0; i < df.n_var; ++i) {
-    if(states[i])
-      df.df[i] = INTEGER(VECTOR_ELT(Df, i));
-    else
-      df.df[i] = REAL(VECTOR_ELT(Df, i));
-  }
-
-  double (* score_fp)(dataframe, int *, int, double *, int *);
-  if(!strcmp(CHAR(STRING_ELT(ScoreType, 0)), BIC_SCORE))
-    score_fp = bic_score;
-  else if (!strcmp(CHAR(STRING_ELT(ScoreType, 0)), BDEU_SCORE))
-    score_fp = bdeu_score;
-  else
-    error("nope\n");
-  double score = score_graph(cg_ptr, df, fargs, iargs, score_fp);
-
-  free(df.df);
-  free_cgraph(cg_ptr);
-  return(ScalarReal(score));
-}
-
-double score_graph(cgraph_ptr cg_ptr, dataframe df, double * fargs, int * iargs,
-  double (* score_fp)(dataframe, int *, int, double *, int *))
+SEXP ccf_score_graph_wrapper(SEXP Graph, SEXP Df, SEXP ScoreType, SEXP States,
+                                         SEXP FloatingArgs, SEXP IntegerArgs)
 {
-  double    score   = 0.0f;
-  int       n_nodes = cg_ptr->n_nodes;
-  ill_ptr * parents = cg_ptr->parents;
-  for(int i = 0; i < n_nodes; ++i) {
-    ill_ptr p = parents[i];
-    if(parents[i]) {
-      int n_par = ill_size(p);
-      int xy[n_par + 1];
-      xy[0] = i;
-      int j = 1;
-      Rprintf("scoring node %i, which has parents", i);
-      while(p) {
-        Rprintf("%i", p->key);
-        xy[j] = p->key;
-        j++;
-        p = p->next;
-      }
-      Rprintf("\n");
-      score += score_fp(df, xy, n_par, fargs, iargs);
+    int    *edges   = calculate_edges_ptr(Graph);
+    int     n_nodes = length(VECTOR_ELT(Graph, NODES));
+    int     n_edges = nrows(VECTOR_ELT(Graph, EDGES));
+    cgraph *cg      = create_cgraph(n_nodes);
+    fill_in_cgraph(cg, n_edges, edges); free(edges);
+    /*
+     * Determine the integer arguments and floating point arguments for the
+     * score function.
+     */
+    int *iargs = NULL;
+    if(!isNull(IntegerArgs))
+        iargs = INTEGER(IntegerArgs);
+    double *fargs = NULL;
+    if(!isNull(FloatingArgs))
+        fargs = REAL(FloatingArgs);
+    /*
+     * states stores the number of states (in the sense of degrees of freedom)
+     * of each variable in the data frame with the caveat that for continuous
+     * variables, the number of states is considered to be to be 0 instead of 1.
+     * This allows me to use the actual value in each entry of dims to determine
+     * the type (real or discrete/integer)of the variable in the data frame.
+     * Instead, we store the columns as void pointers in df. This helps divorce
+     * C and R so it is easier to port this package to python, julia, etc.
+     */
+    dataframe df;
+    df.nvar  = length(Df);
+    df.nobs  = length(VECTOR_ELT(Df, 0));
+    df.states = INTEGER(States);
+    df.df     = malloc(df.nvar * sizeof(void *));
+    /* populate df with the pointers to the columns of the R dataframe */
+    for(int i = 0; i < df.nvar; ++i) {
+        if(df.states[i] != 0)
+            df.df[i] = INTEGER(VECTOR_ELT(Df, i));
+        else
+            df.df[i] = REAL(VECTOR_ELT(Df, i));
     }
-  }
-  Rprintf("total score: %f\n", score);
-  return score;
+
+    score_func score;
+    if(!strcmp(CHAR(STRING_ELT(ScoreType, 0)), BIC_SCORE))
+        score = bic_score;
+    else if (!strcmp(CHAR(STRING_ELT(ScoreType, 0)), BDEU_SCORE))
+        score = bdeu_score;
+    else
+        error("nope\n");
+    double graph_score = score_graph(cg, df, fargs, iargs, score);
+    free(df.df);
+    free_cgraph(cg);
+    return(ScalarReal(graph_score));
 }
-/* score diff calculates the difference in BIC scores between two configurations
-* new and old */
-double score_diff(dataframe df,
-                int * new_xy,
-                int * old_xy,
-                int new_n_par,
-                int old_n_par,
-                double * fargs,
-                int * iargs,
-                double (* score_fp)(dataframe, int *, int, double *, int *)
-              )
+
+/*
+ * score_graph calculates the (BIC-like) score of a graph using a causality
+ * graph, the data associated with the graph, and score function (with
+ * associated floating point args and integer args). Roughly, we use cg
+ * to construct the model x --> y, where x:= Parents(y), and then score
+ * the model given the data.
+ */
+double score_graph(cgraph *cg, dataframe df, double *fargs, int *iargs,
+                                      score_func score)
 {
-  if(old_n_par == 0)
-    return score_fp(df, new_xy, new_n_par, fargs, iargs);
-  else
-    return score_fp(df, new_xy, new_n_par, fargs, iargs)
-             - score_fp(df, old_xy, old_n_par, fargs, iargs);
+    double   graph_score = 0.0f;
+    int      n_nodes     = cg->n_nodes;
+    ill_ptr *parents     = cg->parents;
+    for(int i = 0; i < n_nodes; ++i) {
+        ill_ptr p = parents[i];
+        if(p) {
+            int npar = ill_size(p);
+            int xy[npar + 1];
+            for(int j = 0; j < npar; ++j) {
+                xy[j] = p->key;
+                p     = p->next;
+            }
+            xy[npar + 1] = i; /* set y to i */
+            graph_score += score(df, xy, npar, fargs, iargs);
+        }
+    }
+    return graph_score;
+}
+
+/*
+ * score diff calculates the difference in BIC scores between two configurations
+ * new and old
+ */
+double score_diff(dataframe df, int *new_xy, int *old_xy, int new_npar,
+                                int old_npar, double *fargs, int *iargs,
+                                score_func score)
+{
+    if(old_npar == 0)
+        return score(df, new_xy, new_npar, fargs, iargs);
+    else
+        return score(df, new_xy, new_npar, fargs, iargs)
+               - score(df, old_xy, old_npar, fargs, iargs);
 }
