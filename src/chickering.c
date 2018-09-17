9fc15bba840ed0a793d6924b7a669d37b2b1f48b
tzimiskes <alexanderjrix@gmail.com>
2018-09-17 17:37:48 -0500

    rework chickering.c a bit and reformat pdx.c

--- src/chickering.c
+++ src/chickering.c
@@ -12,174 +12,171 @@
 #define COMPELLED  1 /* This means directed */
 #define REVERSABLE 2 /* This means undirected */
 
-static inline void order_edges(cgraph_ptr cg_ptr, int * sort);
-static inline void insertion_sort(ill_ptr list);
-static inline void chickering_core(cgraph_ptr cg_ptr, int * sort);
+static void order_edges(struct cgraph *cg, int *sort);
+static void insertion_sort(struct ill *list);
+static void find_compelled(struct cgraph *cg, int *sort);
 
 SEXP ccf_chickering_wrapper(SEXP Graph) {
-    int * edges       = calculate_edges_ptr(Graph);
-    int n_nodes       = length(VECTOR_ELT(Graph, NODES));
-    int n_edges       = nrows(VECTOR_ELT(Graph, EDGES));
-    cgraph_ptr cg_ptr = create_cgraph(n_nodes);
-    fill_in_cgraph(cg_ptr, n_edges, edges);
+    int *edges        = calculate_edges_ptr(Graph);
+    int  n_nodes       = length(VECTOR_ELT(Graph, NODES));
+    int  n_edges       = nrows(VECTOR_ELT(Graph, EDGES));
+    struct cgraph *cg = create_cgraph(n_nodes);
+    fill_in_cgraph(cg, n_edges, edges);
     free(edges);
-    TIME_FUNC(chickering_timer, ccf_chickering(cg_ptr));
+    TIME_FUNC(chickering_timer, ccf_chickering(cg));
     SEXP Pattern = PROTECT(duplicate(Graph));
-    recalculate_edges_from_cgraph(cg_ptr, Pattern);
-    free_cgraph(cg_ptr);
+    recalculate_edges_from_cgraph(cg, Pattern);
+    free_cgraph(cg);
     UNPROTECT(1);
     PRINT_TIMER(chickering_timer);
     return Pattern;
 }
 
-void ccf_chickering(cgraph_ptr cg_ptr) {
-  int * sort_ptr = ccf_sort(cg_ptr);
-  order_edges(cg_ptr,     sort_ptr);
-  chickering_core(cg_ptr, sort_ptr);
-  free(sort_ptr);
-  /* we need to recalculate the children after turning it into a pattern.
-   * This is because the children in a cgraph currently only contain the
-   * true children of the graph (ie no undirected edges) */
+void ccf_chickering(struct cgraph *cg)
+{
+    int *sort = ccf_sort(cg);
+    order_edges(cg, sort);
+    find_compelled(cg, sort);
+    free(sort);
 }
 
-/* order_edges orders the parents of cg such that the nodes are in descending
- * order according to the sort. */
-static inline void order_edges(cgraph_ptr cg_ptr, int * sort) {
-  ill_ptr * parents = cg_ptr->parents;
-  int n_nodes       = cg_ptr->n_nodes;
-  /* can be parallelized */
-  for(int i = 0; i < n_nodes; ++i) {
-    ill_ptr tmp = parents[i];
-    while(tmp != NULL) {
-      tmp->value = sort[tmp->key];
-      tmp        = tmp->next;
+/*
+ * order_edges orders the parents of cg such that the nodes are in descending
+ * order according to the sort.
+ */
+static void order_edges(struct cgraph *cg, int *sort)
+{
+    struct ill **parents = cg->parents;
+    int          n_nodes = cg->n_nodes;
+    /* can be parallelized */
+    for(int i = 0; i < n_nodes; ++i) {
+        ill_ptr tmp = parents[i];
+        while(tmp) {
+            tmp->value = sort[tmp->key];
+            tmp        = tmp->next;
+        }
+        insertion_sort(parents[i]);
     }
-    insertion_sort(parents[i]);
-  }
 }
 
-/* We need a sorting routine so we can order the edges. Typically, we would use
+/*
+ * We need a sorting routine so we can order the edges. Typically, we would use
  * a mergesort routine for linked lists, but I suspect insertion sort will be
  * faster because the average degree of causal graphs is 2-5, and insertion sort
- * is faster than merge sort until we hit 10-50 elements. */
-static inline void insertion_sort(ill_ptr list) {
-  while(list != NULL) {
-    ill_ptr top = list;
-    ill_ptr max = list;
-    while(top != NULL) {
-      if(top->value > max->value)
-        max = top;
-      top = top->next;
+ * is faster than merge sort until we hit 10-50 elements.
+ */
+static void insertion_sort(ill *list)
+{
+    while(list) {
+        struct ill *top = list;
+        struct ill *max = list;
+        while(top) {
+            if(top->value > max->value)
+                max = top;
+            top = top->next;
+        }
+        int list_key   = list->key;
+        int list_value = list->value;
+        list->key      = max->key;
+        list->value    = max->value;
+        max->key       = list_key;
+        max->value     = list_value;
+        list           = list->next;
     }
-    int list_key   = list->key;
-    int list_value = list->value;
-    list->key      = max->key;
-    list->value    = max->value;
-    max->key       = list_key;
-    max->value     = list_value;
-    list           = list->next;
-  }
 }
 
-/* This is the core part (i.e, Find-compelled) of Chickering's algorithm
- * to convert DAGs to patterns. */
-static inline void chickering_core(cgraph_ptr cg_ptr, int * sort) {
-  ill_ptr * parents = cg_ptr->parents;
-  int n_nodes       = cg_ptr->n_nodes;
-  /* order edges sets the value parameter for each edge, so we need to
-   * change the value for everything to UNKNOWN */
-  for(int i = 0; i < n_nodes; ++i) {
-    ill_ptr tmp_ptr = parents[i];
-    while(tmp_ptr != NULL) {
-      ill_set_value(tmp_ptr, UNKNOWN);
-      tmp_ptr = ill_next(tmp_ptr);
+static void find_compelled(struct cgraph *cg, int * sort)
+{
+    struct ill **parents = cg->parents;
+    int          n_nodes = cg->n_nodes;
+    /*
+     * order edges sets the value parameter for each edge, so we need to
+     * change the value for everything to UNKNOWN
+     */
+    for(int i = 0; i < n_nodes; ++i) {
+        struct ill *p = parents[i];
+        while(p) {
+            ill_set_value(p, UNKNOWN);
+            p = p->next;
+        }
     }
-  }
-  /* we iterate through the sort to satisfy the max min condition
-   * necessary to run this part of the algorithm */
-  for(int i = 0; i < n_nodes; ++i) {
-    /* by lemma 5 in Chickering, all the incident edges on y are unknown
-     * so we don't need to check to see its unordered */
-    int y             = sort[i];
-    ill_ptr y_parents = parents[y];
-    /* if there are incident edges into y, run steps 5-8 of the algorithm.
-     * if y has no incident edges, go to the next node in the order */
-    if(y_parents != NULL) {
-      int x             = ill_key(y_parents);
-      ill_ptr x_parents =  parents[x];
-      /* for each parent of x, w, where w -> x is compelled
-       * check to see if w forms a chain (w -> x -> y)
-       * or shielded collider (w -> x -> y and w -> x) */
-      while(x_parents != NULL) { /* STEP 5 */
-        if(ill_value(x_parents) == COMPELLED) {
-          int w       = ill_key(x_parents);
-          ill_ptr tmp = y_parents;
-          int chain   = 1;
-          while(tmp != NULL) {
-            /* STEP 7 */
-            if(ill_key(tmp) == w) {
-              /* the triple forms a shielded collider, so set w -> y compelled
-              * and then break out of the loop so we can repeat this for the
-              * next parent of x */
-              ill_set_value(tmp, COMPELLED);
-              chain = 0;
-              break;
+    /*
+     * we iterate through the sort to satisfy the max min condition
+     * necessary to run this part of the algorithm
+     */
+    for(int i = 0; i < n_nodes; ++i) {
+        /* by lemma 5 in Chickering, all the incident edges on y are unknown
+         * so we don't need to check to see its unordered */
+        int         y         = sort[i];
+        struct ill *y_parents = parents[y];
+        /* if y has no incident edges, go to the next node in the order */
+        if(y_parents == NULL)
+            continue;
+        /* Since y has parents, run stepts 5-8 */
+        int         x         = y_parents->key;
+        struct ill *x_parents =  parents[x];
+        /*
+         * for each parent of x, w, where w -> x is compelled
+         * check to see if w forms a chain (w -> x -> y)
+         * or shielded collider (w -> x -> y and w -> x)
+         */
+        while(x_parents) { /* STEP 5 */
+            if(x_parents->value != COMPELLED)
+                goto NEXT;
+            int w = x_parents->key;
+            /* if true , w --> y , x;  x--> y form a shielded collider */
+            if(edge_directed_in_cgraph(cg, w, y)) {
+                x_parents->value = COMPELLED;
             }
-            tmp = ill_next(tmp);
-          }
-          /* STEP 6 */
-          if(chain) {
-            /* reset tmp so we can reiterate through y's parents */
-            tmp = y_parents;
-            while(tmp != NULL) {
-              ill_set_value(tmp, COMPELLED);
-              tmp = ill_next(tmp);
+            /* otherwise it is a chain and parents of y are compelled */
+            else {
+                struct ill *p = y_parents;
+                while(p) {
+                    p->value = COMPELLED;
+                    p        = p->next;
+                }
+                goto EOFL; /* goto end of for loop */
             }
-            goto EOFL; /* goto end of for loop */
-          }
+            /* if step 7 is executed or w isn't compelled, goto the next parent of x */
+            NEXT: ;
+            x_parents = x_parents->next;
         }
-        /* if step 7 is executed, goto the next parent of x */
-        x_parents = ill_next(x_parents);
-      }
-      /* now, we need to search for z, where z -> y, x != z,
-       * and z is not a parent of x. That is, an unshielded collider
-       * by starting at the second parent (might not exist),
-       * we avoid the need to check to see if z = x
-       * STEP 7.5: look for an unshielded collider */
-      int unshielded_collider = 0;
-      ill_ptr tmp = parents[y];
-      while(tmp != NULL) {
-        if(ill_key(tmp) != x) {
-          int z = ill_key(tmp);
-          if(!adjacent_in_cgraph(cg_ptr, x, z)) {
-            unshielded_collider = 1;
-            goto STEP_89;
-          }
+        /* now, we need to search for z, where z -> y, x != z,
+        * and z is not a parent of x. That is, an unshielded collider
+        * by starting at the second parent (might not exist),
+        * we avoid the need to check to see if z = x
+        * STEP 7.5: look for an unshielded collider */
+        int unshielded_collider = 0;
+        struct ill *p = parents[y];
+        while(p) {
+            int z = p->key;
+            if(z != x && !edge_directed_in_cgraph(cg, z, x)) {
+                unshielded_collider = 1;
+                goto STEP_89;
+            }
+            p = p->next;
         }
-        tmp = ill_next(tmp);
-      }
-      STEP_89: {};
-      /* STEP 8: if there is an unshielded collider,
-       * label all incident edges compelled */
-       tmp = parents[y];
-      if(unshielded_collider) {
-        while(tmp != NULL) {
-          ill_set_value(tmp, COMPELLED);
-          tmp = ill_next(tmp);
+        STEP_89: {};
+        /* STEP 8: if there is an unshielded collider,
+        * label all incident edges compelled */
+        p = parents[y];
+        if(unshielded_collider) {
+            while(p) {
+                p->value = COMPELLED;
+                p = p->next;
+            }
         }
-      }
-      /* STEP 9, label all unknown edges reversable */
-      else {
-        while(tmp != NULL) {
-          if(ill_value(tmp) == UNKNOWN) {
-            unorient_directed_edge(cg_ptr, ill_key(tmp), y);
-            tmp = parents[y];
-          }
-          else
-            tmp = ill_next(tmp);
+        /* STEP 9, label all unknown edges reversable */
+        else {
+            while(p) {
+                if(p->value == UNKNOWN) {
+                    unorient_directed_edge(cg, p->key, y);
+                    p = parents[y];
+                }
+                else
+                    p = p->next;
+            }
         }
-      }
+        EOFL:{}; /* End Of For Loop */
     }
-    EOFL:{}; /* End Of For Loop */
-  }
 }
--- src/pdx.c
+++ src/pdx.c
@@ -4,46 +4,45 @@
 #include <edgetypes.h>
 #include <pdx.h>
 
-#define CLIQUE 1
-#define NOCLIQUE 0
+struct cll {
+    struct ill **children;
+    struct ill **parents;
+    struct ill **spouses;
+    struct cll  *next;
+};
 
-typedef struct cll * cll_ptr;
-typedef struct cll {
-  ill_ptr * children;
-  ill_ptr * parents;
-  ill_ptr * spouses;
-  cll_ptr next;
-} cll;
-
-inline int is_sink(cll_ptr node) {
-  return *(node->children) == NULL;
+static int is_sink(struct cll *node)
+ {
+     return *(node->children) == NULL;
 }
 
-/* clique checks each undirected parent of current if that undirected
+/*
+ * clique checks each undirected parent of current if that undirected
  * parent forms a clique with all the other parents of current */
-static int forms_clique(cll_ptr node, cgraph_ptr cg_ptr) {
-  ill_ptr spouses = *(node->spouses);
-  /* grab a spouse (undirected adjacent) */
-  while(spouses != NULL) {
-    int spouse      = ill_key(spouses);
-    ill_ptr parents = *(node->parents);
-    /* make sure spouse is adjacent to the parents of node */
-    while(parents != NULL) {
-      if(!adjacent_in_cgraph(cg_ptr, spouse, ill_key(parents)))
-        return NOCLIQUE;
-      parents = parents->next;
+static int is_clique(struct cll *node, struct cgraph *cg)
+{
+    ill_ptr spouses = *(node->spouses);
+    /* grab a spouse (undirected adjacent) */
+    while(spouses) {
+        int          spouse = spouses->key;
+        struct ill *parents = *(node->parents);
+        /* make sure spouse is adjacent to the parents of node */
+        while(parents) {
+            if(!adjacent_in_cgraph(cg, spouse, parents->key))
+                return 0;
+            parents = parents->next;
+        }
+        /* make sure spouse is adjacent to the other spouses of node */
+        struct ill *p = *(node->spouses);
+        while(p) {
+            int spouse2 = p->key;
+            if(spouse2 != spouse && !adjacent_in_cgraph(cg, spouse, spouse2))
+                return 0;
+            p = p->next;
+        }
+        spouses = spouses->next;
     }
-    /* make sure spouse is adjacent to the other spouses of node */
-    ill_ptr tmp = *(node->spouses);
-    while(tmp != NULL) {
-      int spouse2 = ill_key(tmp);
-      if(spouse2 != spouse && !adjacent_in_cgraph(cg_ptr, spouse, spouse2))
-        return NOCLIQUE;
-      tmp = tmp->next;
-    }
-    spouses = spouses->next;
-  }
-  return CLIQUE;
+    return 1;
 }
 
 static inline void orient_in_cgraph(cgraph_ptr cg_ptr, int node) {
@@ -54,82 +53,85 @@ static inline void orient_in_cgraph(cgraph_ptr cg_ptr, int node) {
   }
 }
 
-void remove_node(cll_ptr current, cll_ptr nodes) {
-  int node = current - nodes; /* ptr arithemtic */
-  /* delete all listings of node in its parents and spouses */
-  ill_ptr parents = *(current->parents);
-  while(parents) {
-    ill_delete(nodes[parents->key].children, node);
-    parents = parents->next;
-  }
-  ill_ptr spouses = *(current->spouses);
-  while(spouses != NULL) {
-    ill_delete(nodes[spouses->key].spouses, node);
-    spouses = spouses->next;
-  }
+void remove_node(struct cll *current, struct cll *nodes)
+{
+    int node = current - nodes; /* ptr arithemtic */
+    /* delete all listings of node in its parents and spouses */
+    struct ill *parents = *(current->parents);
+    while(parents) {
+        ill_delete(nodes[parents->key].children, node);
+        parents = parents->next;
+    }
+    struct ill *spouses = *(current->spouses);
+    while(spouses) {
+        ill_delete(nodes[spouses->key].spouses, node);
+        spouses = spouses->next;
+    }
 }
 
-SEXP ccf_pdx_wrapper(SEXP Pdag) {
-    int * edges_ptr        = calculate_edges_ptr(Pdag);
-    int n_nodes            = length(VECTOR_ELT(Pdag,NODES));
-    int n_edges            = nrows(VECTOR_ELT(Pdag, EDGES));
-    cgraph_ptr cg_ptr      = create_cgraph(n_nodes);
-    fill_in_cgraph(cg_ptr, n_edges, edges_ptr);
+SEXP ccf_pdx_wrapper(SEXP Pdag)
+{
+    int           *edges_ptr = calculate_edges_ptr(Pdag);
+    int            n_nodes   = length(VECTOR_ELT(Pdag,NODES));
+    int            n_edges   = nrows(VECTOR_ELT(Pdag, EDGES));
+    struct cgraph *cg        = create_cgraph(n_nodes);
+    fill_in_cgraph(cg, n_edges, edges_ptr);
     free(edges_ptr);
-    cg_ptr = ccf_pdx(cg_ptr);
-    if(cg_ptr == NULL) {
+    cg = ccf_pdx(cg);
+    if(cg == NULL) {
         return R_NilValue;
     }
     SEXP Dag = PROTECT(duplicate(Pdag));
-    recalculate_edges_from_cgraph(cg_ptr, Dag);
-    free_cgraph(cg_ptr);
+    recalculate_edges_from_cgraph(cg, Dag);
+    free_cgraph(cg);
     UNPROTECT(1);
     return Dag;
 }
 
-cgraph_ptr ccf_pdx(cgraph_ptr cg_ptr) {
-  int n_nodes            = cg_ptr->n_nodes;
-  cgraph_ptr copy_ptr    = copy_cgraph(cg_ptr);
-  cll_ptr nodes          = calloc(n_nodes, sizeof(cll));
-  if(nodes == NULL)
-    error("Failed to allocate memory for nodes in cf_extend_pdag\n");
+cgraph_ptr ccf_pdx(struct cgraph *cg)
+{
+    int            n_nodes = cg->n_nodes;
+    struct cgraph *copy    = copy_cgraph(cg);
+    struct cll    *nodes   = calloc(n_nodes, sizeof(struct cll));
+    if(nodes == NULL)
+        error("Failed to allocate memory for nodes in cf_extend_pdag\n");
     // set up circular linked list
-    ill_ptr * parents  = cg_ptr->parents;
-    ill_ptr * spouses  = cg_ptr->spouses;
-    ill_ptr * children = cg_ptr->children;
+    struct ill **parents  = cg->parents;
+    struct ill **spouses  = cg->spouses;
+    struct ill **children = cg->children;
     for(int i = 0; i < n_nodes; ++i) {
-      nodes[i].parents  = parents  + i;
-      nodes[i].children = children + i;
-      nodes[i].spouses  = spouses  + i;
-      nodes[i].next     = nodes + (i + 1) % n_nodes;
-  }
-  cll_ptr current   = nodes;
-  cll_ptr prev      = nodes + (n_nodes - 1);
-  int n_checked     = 0;
-  int ll_size       = n_nodes;
-  /* Comment needed */
-  while(ll_size > 0 && n_checked < ll_size) {
-    if(is_sink(current) && forms_clique(current, cg_ptr)) {
-      orient_in_cgraph(copy_ptr, current - nodes);
-      remove_node(current, nodes);
-      prev->next = current->next;
-      ll_size--;
-      n_checked = 0;
+        nodes[i].parents  = parents  + i;
+        nodes[i].children = children + i;
+        nodes[i].spouses  = spouses  + i;
+        nodes[i].next     = nodes + (i + 1) % n_nodes;
     }
-    else {
-      n_checked++;
-      prev = prev->next;
+    struct cll *current   = nodes;
+    struct cll *prev      = nodes + (n_nodes - 1);
+    int         n_checked = 0;
+    int         ll_size   = n_nodes;
+    /* Comment needed */
+    while(ll_size > 0 && n_checked <= ll_size) {
+        if(is_sink(current) && is_clique(current, cg)) {
+            orient_in_cgraph(copy, current - nodes);
+            remove_node(current, nodes);
+            prev->next = current->next;
+            ll_size--;
+            n_checked = 0;
+        }
+        else {
+            n_checked++;
+            prev = prev->next;
+        }
+        current = current->next;
     }
-    current = current->next;
-  }
-  free(nodes);
-  free_cgraph(cg_ptr);
-  /* check to see if pdx failed to generate an extension. If there is a
-   * failure, free the copy_ptr and set it to NULL. */
-  int failure = ll_size  > 0 ? 1 : 0;
-  if(failure) {
-    free_cgraph(copy_ptr);
-    copy_ptr = NULL;
-  }
-  return copy_ptr;
+    free(nodes);
+    free_cgraph(cg);
+    /* check to see if pdx failed to generate an extension. If there is a
+    * failure, free the copy_ptr and set it to NULL. */
+    int failure = ll_size  > 0 ? 1 : 0;
+    if(failure) {
+        free_cgraph(copy);
+        copy = NULL;
+    }
+    return copy;
 }
