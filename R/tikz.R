0993f47e31bbd93d9a396a70c203c856c7c1ca19
tzimiskes <alexanderjrix@gmail.com>
2018-11-27 07:09:18 -0600

    add FR algorithm for tikz exporting. Not quite working

--- R/tikz.R
+++ R/tikz.R
@@ -1,7 +1,16 @@
-(* should be fib(n-1) + fib(n-2) *) 
-rt_tikz <- function(cgraph) {
-  
-  match_edge <- function(edge) {
+#' @useDynLib causality causalityFruchtermanReingold
+#' @export
+export_tikz <- function(cgraph, file = stdout(), iterations = 10, height = 3,
+                                width = 3)
+{
+
+
+  positions <- .Call("causalityFruchtermanReingold", graph = cgraph,
+                                                     nIterations = iterations,
+                                                     height = height,
+                                                     width = width)
+  print(positions)
+  edge_style <- function(edge) {
     switch (edge,
       "-->" = "directed",
       "---" = "undirected",
@@ -15,38 +24,41 @@ rt_tikz <- function(cgraph) {
   preamble <- c("\\documentclass[border = 3px]{standalone}\n",
                 "\\usepackage{tikz}\n",
                 "\\usetikzlibrary{shapes, arrows.meta}\n")
-  cat(preamble, file = stdout(), sep = "")
-  
-  cat("\\begin{document}\n", file = stdout(), append = T)
-  cat("\\begin{tikzpicture}[{shorten >=1pt, >=stealth, scale = 1.5}]\n", file = stdout(), append = T) 
-  
-  tikz.defs <- c("\\tikzstyle{node}          = [draw, fill = white, circle, scale =.75, line width = .7]",
+  cat(preamble, file = file, sep = "")
+
+  cat("\\begin{document}\n", file = file, append = T)
+  cat("\\begin{tikzpicture}[{shorten >=1pt, >=stealth, scale = 1.5}]\n", file = file, append = T)
+
+  tikz.defs <- c("\\tikzstyle{node}          = [draw, fill = white, circle, scale =.75, line width = .7]\n",
                  "\\tikzstyle{directed}      = [-{Latex[length = 2.5mm]}]\n",
                  "\\tikzstyle{circ-directed} = [{Circle[open, length=2.5mm]}-{Latex[length = 3mm]}]\n",
                  "\\tikzstyle{circ-circ}     = [{Circle[open, length = 2.5mm]}-{Circle[open, length = 2.5mm]}]\n",
                  "\\tikzstyle{circ-circ}     = [{Circle[open, length = 2.5mm]}-{Circle[open, length = 2.5mm]}]\n",
                  "\\tikzstyle{bidirected}    = [{Latex[length=3mm]}-{Latex[length=3mm]}]\n",
                  "\\tikzstyle{certain}       = [green!85!black]\n",
-                 "\\tikzstyle{uncertain}     = [dashed, black]\n")  
+                 "\\tikzstyle{uncertain}     = [dashed, black]\n")
 
-  cat(tikz.defs, file = stdout(), sep = "")
+  cat(tikz.defs, file = file, sep = "", append = T)
   nodes <- cgraph$nodes
-  for (node in nodes)
-    cat(sprintf("\\node[node] at (0, 0) (%s) {%s};\n", node, node), 
-        file = stdout(), append = T)
-  
-  cat("\n", file= stdout(), append = T)
-  edges <- cgraph$edges 
+  for (node in nodes) {
+      x <- positions[match(node, nodes), 1]
+      y <- positions[match(node, nodes), 2]
+      if (!is.null(cgraph$adjacencies[[node]]))
+        cat(sprintf("\\node[node] at (%f, %f) (%s) {%s};\n", x, y, node, node),
+                      file = file, append = T)
+
+  }
+
+  cat("\n", file = file, append = T)
+  edges <- cgraph$edges
   for (i in 1:nrow(edges)) {
       x    <- edges[i, 1]
       y    <- edges[i, 2]
-      edge.type <- match_edge(edges[i, 3])
+      edge.type <- edge_style(edges[i, 3])
       s <- sprintf("\\draw[%s, line width = 1] (%s) -- (%s);\n", edge.type, x, y)
-      cat(s, file = stdout(), append = T)
+      cat(s, file = file, append = T)
   }
-  cat("\\end{tikzpicture}\n", file = stdout(), append = T) 
-  cat("\\end{document}\n", file = stdout(), append = T)
-  
+  cat("\\end{tikzpicture}\n", file = file, append = T)
+  cat("\\end{document}\n", file = file, append = T)
+
 }
-
-
--- src/fruchterman_reingold.c
+++ src/fruchterman_reingold.c
@@ -1,123 +1,104 @@
 #include <stdlib.h>
+#include <float.h>
 #include <math.h>
-#include <R.h>
-#include <Rmath.h>
+#include <time.h>
+#include "headers/causality.h"
+#include "headers/causalityRWrapper.h"
 
-void qgraph_layout_fruchtermanreingold_R(int *pniter, int *pvcount, int *pecount,
-	double *maxdelta,
-	double *parea,
-	double *pcoolexp,
-	double *prepulserad,
-	int *Ef,  /* Edges from */
-	int *Et, /*Edges t0*/
-	double *W,
-	double *x,
-	double *y,
-	int *Cx,
-	int *Cy) {
-/*
-Calculate a two-dimensional Fruchterman-Reingold layout for (symmetrized)
-edgelist matrix d.  Positions (stored in (x,y)) should be initialized
-prior to calling this routine.
-*/
+void ccf_fruchterman_reingold(double *positions, int n_nodes, int *edges,
+                                                 int n_edges, double width,
+                                                 double height,
+                                                 int n_iterations);
 
-int n;
-int m;
-double frk;
-double *t;
-double ded;
-double xd;
-double yd;
-double *dx;
-double *dy;
-double rf;
-double af;
-int i;
-int j;
-int k;
-int l;
-int niter;
-//double maxdelta;
-double area;
-double coolexp;
-double repulserad;
+SEXP causalityFruchtermanReingold(SEXP graph, SEXP nIterations, SEXP height,
+                                               SEXP width)
+{
 
-n = (int)*pvcount;
-m = (int)*pecount;
-niter=*pniter;
-//maxdelta=*pmaxdelta;
-area=*parea;
-coolexp=*pcoolexp;
-repulserad=*prepulserad;
+    int nNodes     = length(VECTOR_ELT(graph, NODES));
+    int nEdges     = nrows(VECTOR_ELT(graph, EDGES));
+    SEXP positions = PROTECT(allocMatrix(REALSXP, nNodes, 2));
+
+    int *edges = calculateEdgesPtr(graph);
+
+    ccf_fruchterman_reingold(REAL(positions), nNodes, edges, nEdges,
+                                              asReal(width), asReal(height),
+                                              asInteger(nIterations));
+    free(edges);
+    UNPROTECT(1);
+    return positions;
+}
 
 
-/*Allocate memory for transient structures*/
-dx=(double *)R_alloc(n,sizeof(double));
-dy=(double *)R_alloc(n,sizeof(double));
-t=(double *)R_alloc(n,sizeof(double));
+void ccf_uniform_rng(double *x, int n, double low, double high)
+{
+    #ifdef R_R_H
+    GetRNGstate();
+    for (int i = 0; i < n; ++i)
+        x[i] = low + (high - low) * unif_rand();
+    PutRNGstate();
+    #else
+    srand();
+    for (int i = 0; i < n; ++i)
+        x[i] = low  + (high - low) * rand()/ RAND_MAX;
+    #endif
+}
 
-frk=sqrt(area/(double)n);
+void ccf_fruchterman_reingold(double *positions, int n_nodes, int *edges,
+                                                     int n_edges, double width,
+                                                     double height,
+                                                     int n_iterations)
+{
+    double  area       = height * width;
+    double  inv_k      = 1.0f / sqrt(area/n_nodes);
+    double  k_sq       = area/n_nodes;
+    double *x          = positions;
+    double *y          = positions + n_nodes;
+    double *velocities = malloc(2 * n_nodes * sizeof(double));
+    double *v_x        = velocities;
+    double *v_y        = velocities + n_nodes;
+    /* give each node an initial position in space */
+    ccf_uniform_rng(x, n_nodes, 0.0f, width);
+    ccf_uniform_rng(y, n_nodes, 0.0f, height);
 
-  /*Run the annealing loop*/
-  for(i=niter;i>=0;i--){
-        /*Clear the deltas*/
-    for(j=0;j<n;j++){
-      dx[j]=0.0;
-      dy[j]=0.0;
+    for (int i = 0; i < n_nodes; ++i)
+        Rprintf("%f %f\n", x[i], y[i]);
+    double t0 = 2 * (width * height) / (width + height);
+    double t  = t0;
+    for (int i = 0; i < n_iterations; ++i) {
+        /* calculate the dispersive forces between x_j and x_k (j != k) */
+        for (int j = 0; j < n_nodes; ++j) {
+            v_x[j] = 0.0f;
+            v_y[j] = 0.0f;
+            for (int k = 0; k < n_nodes; ++k) {
+                if (j == k)
+                    continue;
+                double dx [2]  = {x[j] - x[k], y[j] - y[k]};
+                double norm_sq = dx[0] * dx[0] + dx[1] * dx[1] + 1e-6;
+                v_x[j] += dx[0] / norm_sq * k_sq;
+                v_y[j] += dx[1] / norm_sq * k_sq;
+            }
+        }
+
+        for (int j = 0; j < n_edges; ++j) {
+            int n1 = edges[j];
+            int n2 = edges[j + n_edges];
+            double dx [2] = {x[n1] - x[n2], y[n1] - y[n2]};
+            double norm   = sqrt(dx[0] * dx[0] + dx[1] * dx[1]);
+            v_x[n1] -= dx[0] * norm * inv_k;
+            v_y[n1] -= dx[1] * norm * inv_k;
+            v_x[n2] += dx[0] * norm * inv_k;
+            v_y[n2] += dx[1] * norm * inv_k;
+        }
+
+        for (int j = 0; j < n_nodes; ++j) {
+            double s = v_x[j] * v_x[j] + v_y[j] * v_y[j];
+            s = sqrt(s + 1e-6);
+            x[j] += v_x[j] * fmin(1.0f, t / s);
+            y[j] += v_y[j] * fmin(1.0f, t / s);
+            x[j] = fmin(width, fmax(0.0f, x[j]));
+            y[j] = fmin(height, fmax(0.0f, y[j]));
+        }
+        t = t0 * (n_iterations - i + 1) / n_iterations;
     }
-    /*Increment deltas for each undirected pair*/
-    for(j=0;j<n;j++)
-	{
-		/*Set the temperature (maximum move/iteration)*/
-		t[j]=maxdelta[j]*pow(i/(double)niter,coolexp);
-
-		for(k=j+1;k<n;k++){
-			/*Obtain difference vector*/
-			xd=x[j]-x[k];
-			yd=y[j]-y[k];
-			ded=sqrt(xd*xd+yd*yd);  /*Get dyadic euclidean distance*/
-			xd/=ded;                /*Rescale differences to length 1*/
-			yd/=ded;
-			/*Calculate repulsive "force"*/
-			rf=frk*frk*(1.0/ded-ded*ded/repulserad);
-			dx[j]+=xd*rf;        /*Add to the position change vector*/
-			dx[k]-=xd*rf;
-			dy[j]+=yd*rf;
-			dy[k]-=yd*rf;
-		}
-	}
-    /*Calculate the attractive "force"*/
-    for(j=0;j<m;j++){
-      k=Ef[j];
-      l=Et[j];
-
-      xd=x[k]-x[l];
-      yd=y[k]-y[l];
-      ded=sqrt(xd*xd+yd*yd);  /*Get dyadic euclidean distance*/
-      if (ded != 0)
-	  {
-		xd/=ded;                /*Rescale differences to length 1*/
-		yd/=ded;
-	  }
-      af=ded*ded/frk*W[j];
-      dx[k]-=xd*af;        /*Add to the position change vector*/
-      dx[l]+=xd*af;
-      dy[k]-=yd*af;
-      dy[l]+=yd*af;
-
-    }
-    /*Dampen motion, if needed, and move the points*/
-    for(j=0;j<n;j++){
-      ded=sqrt(dx[j]*dx[j]+dy[j]*dy[j]);
-      if(ded>t[j]){                 /*Dampen to t*/
-        ded=t[j]/ded;
-        dx[j]*=ded;
-        dy[j]*=ded;
-      }
-	  if (!Cx[j])
-       x[j]+=dx[j];               /*Update positions*/
-      if (!Cy[j])
-	   y[j]+=dy[j];
-    }
-  }
+    free(velocities);
 }
