ce6a46b227dbb101de4d3e0c9d9ee04a852bbb5d
tzimiskes <alexanderjrix@gmail.com>
2019-01-14 12:40:50 -0600

    remove ges info from scores.h and move it into ges.h

--- src/headers/scores.h
+++ src/headers/scores.h
@@ -13,26 +13,14 @@ struct score_args {
 typedef double (*score_func)(struct dataframe df, int *xy, int npar,
                                                   struct score_args args);
 
-typedef double (*ges_score_func)(struct dataframe df, int x, int y, int *ypar,
-                                                      int npar,
-                                                      struct score_args args,
-                                                      double *fmem, int *imem);
-
 double bdeu_score(struct dataframe df, int *xy, int npar,
                                        struct score_args args);
 
-double ges_bdeu_score(struct dataframe data, int x, int y, int *ypar, int npar,
-                                             struct score_args args,
-                                             double *fmem, int *imem);
 
 double bic_score(struct dataframe df, int *xy, int npar,
                                       struct score_args args);
 
-double ges_bic_score(struct dataframe data, int x, int y, int *ypar, int npar,
-                                            struct score_args args,
-                                            double *fmem, int *imem);
-
-
-void fcov_xx(double *cov_xx, double **x, int n, int m);
-void fcov_xy(double *cov_xy, double **x, double *y, int n, int m);
+void   fcov_xx(double *cov_xx, double **x, int n, int m);
+void   fcov_xy(double *cov_xy, double **x, double *y, int n, int m);
+double calculate_rss(double *cov, int m);
 #endif
