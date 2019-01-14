b65b0a9ebfe3e94868beb0190886b6c92d7aed01
tzimiskes <alexanderjrix@gmail.com>
2019-01-14 12:41:43 -0600

    add ges score functino info from scores.h and add new struct to hold into memory related info for continuous scores.

--- src/headers/ges.h
+++ src/headers/ges.h
@@ -1,5 +1,6 @@
 #include <stdint.h>
 
+#include "cgraph.h"
 #include "scores.h"
 #include "dataframe.h"
 
@@ -9,12 +10,32 @@
 #define INSERTION 1
 #define DELETION  0
 
+struct ges_score_mem {
+    double *cov_xy;
+    double *cov_xx;
+    double *cov_xpx;
+    int    *lbls;
+    int     m;
+};
+
+typedef double (*ges_score_func)(struct dataframe df, int x, int y, int *ypar,
+                                                      int npar,
+                                                      struct score_args args,
+                                                      struct ges_score_mem gsm);
+
+double ges_bdeu_score(struct dataframe data, int x, int y, int *ypar, int npar,
+                                             struct score_args args,
+                                             struct ges_score_mem gsm);
+
+double ges_bic_score(struct dataframe data, int x, int y, int *ypar, int npar,
+                                            struct score_args args,
+                                            struct ges_score_mem gsm);
+
 struct ges_score {
-    ges_score_func    score;
-    struct dataframe  df;
-    struct score_args args;
-    double           *fmem;
-    int              *imem;
+    ges_score_func       gsf;
+    struct ges_score_mem gsm;
+    struct dataframe     df;
+    struct score_args    args;
 };
 
 struct ges_op {
@@ -32,19 +53,26 @@ struct ges_op {
     int    set_size;
     int    type;
     double score_diff;
-}; /*  64 bytes */
+}; /* 64 bytes */
 
+/* memory utility functions */
 void free_ges_op(struct ges_op op);
-void free_ges_score(struct ges_score sc);
+void free_ges_score_mem(struct ges_score_mem gsm);
+/* functions that deterimine whether or not a operators is legal */
 int  valid_fes_clique(struct cgraph *cg, struct ges_op op);
 int  valid_bes_clique(struct cgraph *cg, struct ges_op op);
 int  cycle_created(struct cgraph *cg, struct ges_op op, int *mem);
+/* misc utility functions */
 void partition_neighbors(struct cgraph *cg, struct ges_op *op);
 void calculate_naxy(struct cgraph *cg, struct ges_op *op);
 void calculate_parents(struct cgraph *cg, struct ges_op *op);
+/* reorient cgraph after an operator has been applied */
 void reorient(struct cgraph *cg, struct ges_op op, int *visited, int *n);
 void reorient_and_determine_operators_to_update(struct cgraph *cpy,
                                                 struct cgraph *cg,
                                                 struct ges_op op,
                                                 int *nodes, int *n);
+/* functions that optimize a score */
+void ges_bic_opt1(struct cgraph *cg, int y, int n, struct ges_score *gs);
+void ges_bic_opt2(struct cgraph *cg, int xp, struct ges_score *gs);
 #endif
