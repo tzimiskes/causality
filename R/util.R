c24e3b792ce69f3e635a160b3f91a6ec0fd56349
tzimiskes <alexanderjrix@gmail.com>
2019-03-12 11:25:24 -0500

    remove commented out code

--- R/util.R
+++ R/util.R
@@ -19,39 +19,3 @@
   names(adjacencies) <- nodes
   return(adjacencies)
 }
-
-# I'm pretty sire this code is garbage (arix)
-.prepare_cgraph_for_call <- function(cgraph, nodes, edges, adjacencies) {
-  hash <- list()
-  for (i in 1:length(cgraph$nodes))
-    hash[[cgraph$nodes[[i]]]] <- i - 1
-
-  if(edges) {
-    for (i in 1:nrow(cgraph$edges)) {
-      cgraph$edges[i, 1] <- hash[[cgraph$edges[i, 1]]]
-      cgraph$edges[i, 2] <- hash[[cgraph$edges[i, 2]]]
-      cgraph$edges[i, 3] <-
-      switch(cgraph$edges[i, 3],
-      # it is important that these edge/number pairs match up with edgetypes.h
-             "-->" = 1,
-             "---" = 2,
-             "++>" = 3,
-             "~~>" = 4,
-             "o->" = 5,
-             "o-o" = 6,
-             "<->" = 7)
-    }
-    nc <- ncol(cgraph$edges)
-    nr <- nrow(cgraph$edges)
-    cgraph$edges <- as.integer(cgraph$edges)
-    dim(cgraph$edges) <- c(nr, nc)
-  }
-  if (adjacencies) {
-    cgraph$adjacencies <- lapply(cgraph$adjacencies, function(x) {
-      as.integer(unname(unlist(hash)[x]))
-    })
-  }
-  if (nodes)
-    cgraph$nodes <- unname(unlist(hash))
-  return(cgraph)
-}
