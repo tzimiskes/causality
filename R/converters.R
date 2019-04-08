c8549a35d94d9de34c77b2a9f756f29dd44c9993
Alexander Rix <alexanderjrix@gmail.com>
2019-04-08 13:51:40 -0500

    work on documentation

--- R/converters.R
+++ R/converters.R
@@ -1,8 +1,21 @@
-# converters.R contains interfaces to causality c functions to help convert the
-# different cgraph subclasses into each other
-# Author: Alexander Rix
-
-
+#' Turn a causality DAG into a pattern.
+#'
+#' \code{chickering} converts dags into their markov equivalence class. This
+#' function exists mostly to provide documentation on how causality dags are
+#' converted to patterns (or pdags). Using \code{as.pattern} instead is
+#' recommended.
+#' @param graph a causality dag.
+#' @return a causality.pattern
+#' @details \code{chickering} generates a topological sort of the dag, and
+#'     generates an ordering of the edges based off this topological sort. Then,
+#'     every edge is marked as 'unkown' and algorithm iterates through the nodes 
+#'     acoording to their order. If there is an unshielded collider on a node,
+#'     all of its parents are marked as directed.
+#' @references
+#'    Chickering DM. A transformational characterization of equivalent Bayesian
+#'    network structures. InProceedings of the Eleventh conference on
+#'    Uncertainty  in artificial intelligence 1995 Aug 18 (pp. 87-98). Morgan
+#'    Kaufmann Publishers Inc. \href{http:///arxiv.org/abs/1302.4938}{arXiv:1302.4938 [cs.AI]}.
 #' @export
 chickering <- function(graph)
 {
