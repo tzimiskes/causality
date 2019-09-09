7c7b7c252dba881b5b0c9da74f87c06fd255d639
Alex Rix <alexanderjrix@gmail.com>
2019-09-09 14:51:03 -0400

    fix formating

--- R/util.R
+++ R/util.R
@@ -1,21 +1,22 @@
 # get the skeleton from the edge representation
 # loop over the nodes
-.calculate_adjacencies <- function(edges, nodes) {
+.calculate_adjacencies <- function(edges, nodes)
+{
   n.edges <- nrow(edges)
-  adjacencies <- lapply(nodes, function(node) {
+  adjacencies <- lapply(nodes, function(node)
+  {
     neighborhood <- c()
     # loop over the edges
     for (i in 1:n.edges) {
       # if a node is in an edge, find its partner (neighbor)
-      if( node %in% edges[i, 1:2]) {
+      if (node %in% edges[i, 1:2]) {
         neighbor <- edges[i, c(node != edges[i,1:2], F)]
         if (!(neighbor %in% neighborhood))
           neighborhood <- c(neighborhood, neighbor)
       }
     }
-    return(neighborhood)
-  }
-  )
+    neighborhood
+  })
   names(adjacencies) <- nodes
-  return(adjacencies)
+  adjacencies
 }
