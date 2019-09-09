072e9aa8e3db46204d8bdc53315c8fa32a08e7ec
Alex Rix <alexanderjrix@gmail.com>
2019-09-09 15:14:27 -0400

    merge util.R

--- R/util.R
+++ R/util.R
@@ -2,21 +2,20 @@
 # loop over the nodes
 .calculate_adjacencies <- function(edges, nodes)
 {
-  n.edges <- nrow(edges)
-  adjacencies <- lapply(nodes, function(node)
-  {
-    neighborhood <- c()
-    # loop over the edges
-    for (i in 1:n.edges) {
-      # if a node is in an edge, find its partner (neighbor)
-      if (node %in% edges[i, 1:2]) {
-        neighbor <- edges[i, c(node != edges[i,1:2], F)]
-        if (!(neighbor %in% neighborhood))
-          neighborhood <- c(neighborhood, neighbor)
-      }
-    }
-    neighborhood
-  })
-  names(adjacencies) <- nodes
-  adjacencies
+    n.edges <- nrow(edges)
+    adjacencies <- lapply(nodes, function(node) {
+        neighborhood <- c()
+        # loop over the edges
+        for (i in 1:n.edges) {
+            # if a node is in an edge, find its partner (neighbor)
+            if( node %in% edges[i, 1:2]) {
+                neighbor <- edges[i, c(node != edges[i, 1:2], F)]
+                if (!(neighbor %in% neighborhood))
+                    neighborhood <- c(neighborhood, neighbor)
+            }
+        }
+        return(neighborhood)
+    })
+    names(adjacencies) <- nodes
+    return(adjacencies)
 }
